f8633f75bfeb2b5772d017173f302fdaa1afa6e6
williamrolando88 <william.rolando88@hotmail.com>
2021-12-15 17:22:12 -0500

    Delete Scores set method

--- src/index.js
+++ src/index.js
@@ -2,3 +2,22 @@ import addScores from './modules/addScores.js';
 import './styles.css';
 
 document.querySelector('form').addEventListener('submit', addScores);
+
+async function refresh() {
+  let response = await fetch(
+    'https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/kOkmDn5UlSKIBKnC0NtR/scores/',
+    {
+      method: 'GET',
+      headers: { 'Content-Type': 'application/json' },
+    },
+  );
+  let scores = response.json();
+  console.log(scores);
+}
+
+document.querySelector('#refresh').addEventListener('click', (e) => {
+  e.preventDefault();
+  refresh();
+});
+
+// kOkmDn5UlSKIBKnC0NtR/scores/
--- src/modules/Scores.js
+++ src/modules/Scores.js
@@ -9,10 +9,5 @@ export default class Scores {
       score,
     };
     this.array.push(element);
-    this.set();
-  }
-
-  set() {
-    localStorage.setItem('scoresTable', JSON.stringify(this.array));
   }
 }
