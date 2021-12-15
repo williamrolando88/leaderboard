674af50f991bd8bc69b612684177e73d3889e3d0
williamrolando88 <william.rolando88@hotmail.com>
2021-12-15 07:45:26 -0500

    Refactor index.js

--- src/index.js
+++ src/index.js
@@ -1,47 +1,4 @@
+import addScores from './modules/addScores';
 import './styles.css';
 
-class Scores {
-  constructor() {
-    this.array = [];
-  }
-
-  add(name, score) {
-    const element = {
-      name: name,
-      score: score,
-    };
-    this.array.push(element);
-    this.set();
-  }
-
-  set() {
-    localStorage.setItem('scoresTable', JSON.stringify(this.array));
-  }
-}
-
-const scoreTable = new Scores();
-
-function addScores(e) {
-  e.preventDefault();
-  const name = document.querySelector('#name');
-  const score = document.querySelector('#score');
-  scoreTable.add(name.value, score.value);
-  name.value = '';
-  score.value = '';
-}
-
-function render(element) {
-  const scoreEntry = document.createElement('tr');
-
-  const nameTd = document.createElement('td');
-  nameTd.value = element.name;
-  scoreEntry.appendChild(nameTd);
-
-  const scoreTd = document.createElement('td');
-  scoreTd.value = element.score;
-  scoreEntry.appendChild(scoreTd);
-
-  return scoreEntry;
-}
-
-document.querySelector('form').addEventListener('submit');
+document.querySelector('form').addEventListener('submit', addScores);
