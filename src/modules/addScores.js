9540ef3815ef6b06b08bb882e83336757939f6a0
williamrolando88 <william.rolando88@hotmail.com>
2021-12-16 10:13:10 -0500

    Add function to post scores in API

--- src/modules/addScores.js
+++ src/modules/addScores.js
@@ -1,4 +1,5 @@
 import appendElements from './appendElements.js';
+import postScores from './postScores.js';
 import Scores from './Scores.js';
 
 const scoreTable = new Scores();
@@ -8,8 +9,8 @@ export default function addScores(e) {
   const name = document.querySelector('#name');
   const score = document.querySelector('#score');
   scoreTable.add(name.value, score.value);
+  postScores(name.value, score.value);
   name.value = '';
   score.value = '';
   name.focus();
-  appendElements(scoreTable.array);
 }
